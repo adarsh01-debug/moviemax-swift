3af6e42b123c11e95f2afab9eda26dad8d233721
Adarsh Pandey <adarsh.pandey@gdn-commerce.com>
2022-09-20 18:23:11 +0530

    placed each component in seperate xib

---
+++ MovieMax/View/WatchListButtonTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  WatchListButtonTableViewCell.swift
+//  MovieMax
+//
+//  Created by Adarsh Pandey on 20/09/22.
+//
+
+import UIKit
+
+class WatchListButtonTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
